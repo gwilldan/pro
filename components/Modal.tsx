9173eca9d7a9ef2a5f90101f4f49c5f71a48b01c
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-08-30 04:14:29 +0100

    my brain wan blowww

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -48,25 +48,23 @@ export default function Modal(): JSX.Element {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden  bg-white p-12 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
+                   
                     
                   </Dialog.Title>
                   <div className="mt-2">
-                   
+                  <div className="flex justify-between items-center">
+                    <h4 className="px-6 ">Sign Up</h4>
+                    <h4>Login</h4>
+                  </div>
                   </div>
 
                   <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      
-                    </button>
+                    
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
