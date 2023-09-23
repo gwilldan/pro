80f22ed997228a6d4d911cc20fb76c3bba1f4d24
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-09-23 15:35:46 +0100

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,12 +9,13 @@ import Search from "./Search";
 import { IconContext } from "react-icons/lib/esm/iconContext";
 import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
-import filter from "../assets/filter.svg"
+import filter from "../assets/filter.svg";
 import Image from "next/image";
 
 const Hero: React.FC = () => {
   const [contentWidth, setContentWidth] = useState<number>(0);
   const [viewportWidth, setViewportWidth] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -55,10 +56,35 @@ const Hero: React.FC = () => {
           <div className="w-[85%] h-[40px]  border-[1px] border-[#F4F4F4] rounded-[8px] pl-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
             <input className="w-[100%] h-[100%] outline-none  " type="text" />
           </div>
-          <div className="w-[12%] h-[40px]  border-[1px] border-[#F4F4F4] rounded-[8px]  shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)] flex justify-center items-center">
-           <Image src={filter} width={24} height={24} alt="filter"/>
-            
-          </div>
+          
+            <div
+              onClick={() => setIsOpen((prev) => !prev)}
+              className=" relative cursor-pointer w-[12%] h-[40px]  border-[1px] border-[#F4F4F4] rounded-[8px]  shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)] flex justify-center items-center"
+            >
+              <div>
+              <Image src={filter} width={24} height={24} alt="filter" />
+              </div>
+       
+           
+            {isOpen && (
+              <div className="absolute bg-white shadow-[0_4px_6px_-2px_rgba(0,0,0,0.3)] rounded-[4px] bottom-0 left-0  flex col items-start p-2 w-[100%]">
+                <div className="w-[100%] flex flex-col">
+                  <div className="hover:bg-blue-300 cursor-pointer border-l-transparent hover:border-l-white border-l-4 w-full ">
+                    <h6>1</h6>
+                  </div>
+                  <div className="hover:bg-blue-300 cursor-pointer border-l-transparent hover:border-l-white border-l-4 w-full ">
+                    <h6>1</h6>
+                  </div>
+                  <div className="hover:bg-blue-300 cursor-pointer border-l-transparent hover:border-l-white border-l-4 w-full ">
+                    <h6>1</h6>
+                  </div>
+                  <div className="hover:bg-blue-300 cursor-pointer border-l-transparent hover:border-l-white border-l-4 w-full ">
+                    <h6>1</h6>
+                  </div>
+                </div>
+              </div>
+            )}
+           </div>
         </div>
         <div className="flex md:hidden">
           <motion.div>
@@ -84,89 +110,90 @@ const Hero: React.FC = () => {
         </div>
       </div>
       <div className="md:flex hidden">
-      <div className="md:px-16 px-6 mt-20">
-        <div className="flex justify-between items-center px-3  border-b-[1px] border-[#F4F4F4] ">
-          <div className="flex gap-10 text-base  text-secondary ">
-            <h5>All Categories</h5>
-            <h5>Websites</h5>
-            <h5>Mobile Apps</h5>
-            <h5>Web Apps</h5>
-            <h5>Others</h5>
-          </div>
-          <div className="flex gap-5">
-            <div className="flex gap-2  justify-center items-center border border-[#393A32] px-6 py-3 rounded">
-              <BsIcons.BsFilter />
-              <h6 className="text-[16px]">Filter</h6>
+        <div className="md:px-16 px-6 mt-20">
+          <div className="flex justify-between items-center px-3  border-b-[1px] border-[#F4F4F4] ">
+            <div className="flex gap-10 text-base  text-secondary ">
+              <h5>All Categories</h5>
+              <h5>Websites</h5>
+              <h5>Mobile Apps</h5>
+              <h5>Web Apps</h5>
+              <h5>Others</h5>
             </div>
-            <div className="flex gap-2 items-center border border-[#393A32] px-6 py-3 rounded">
-              <h6 className="text-[16px]">Sort by</h6>
+            <div className="flex gap-5">
+              <div className="flex gap-2  justify-center items-center border border-[#393A32] px-6 py-3 rounded">
+                <BsIcons.BsFilter />
+                <h6 className="text-[16px]">Filter</h6>
+              </div>
+              <div className="flex gap-2 items-center border border-[#393A32] px-6 py-3 rounded">
+                <h6 className="text-[16px]">Sort by</h6>
 
-              <RxIcons.RxCaretDown />
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <div className="flex justify-between items-center mt-8 ">
-            <Search />
-
-            <div className="w-[265px] h-[74px] gap-2 sm:flex hidden justify-between flex-col">
-              <div className="w-[100%] flex justify-between  items-center">
-                <h6 className=" text-[14px]">Category</h6>
-              </div>
-              <div className=" flex justify-between  items-center w-full h-full border-[1px] border-[#F4F4F4] rounded-lg px-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
-                <h6 className=" text-[14px]">Option One</h6>
-                <PiIcons.PiCaretDownBold />
-              </div>
-            </div>
-            <div className="w-[265px] h-[74px] gap-2 sm:flex hidden justify-between flex-col">
-              <div className="w-[100%] flex justify-between  items-center">
-                <h6 className=" text-[14px]">Industry/Niche</h6>
-              </div>
-              <div className=" flex justify-between  items-center w-full h-full border-[1px] border-[#F4F4F4] rounded-lg px-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
-                <h6 className=" text-[14px]">Option One</h6>
-                <PiIcons.PiCaretDownBold />
-              </div>
-            </div>
-            <div className="w-[265px] h-[74px] gap-2 sm:flex hidden justify-between flex-col">
-              <div className="w-[100%] flex justify-between  items-center">
-                <h6 className=" text-[14px]">Difficulty Level</h6>
-              </div>
-              <div className=" flex justify-between  items-center w-full h-full border-[1px] border-[#F4F4F4] rounded-lg px-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
-                <h6 className=" text-[14px]">Option One</h6>
-                <PiIcons.PiCaretDownBold />
+                <RxIcons.RxCaretDown />
               </div>
             </div>
           </div>
+          <div className="">
+            <div className="flex justify-between items-center mt-8 ">
+              <Search />
 
-          <div >
-            <div className="flex justify-between items-baseline">
-              <div className="my-8 flex gap-2">
-                <div className="flex bg-[#F4F4F4] py-2 pl-4 pr-3 gap-3 justify-center items-center w-[80px] rounded-lg shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
-                  <h5 className="text-base">Tag</h5>
-                  <IconContext.Provider value={{ size: "15px" }}>
-                    <IoIcons.IoMdClose />
-                  </IconContext.Provider>
+              <div className="w-[265px] h-[74px] gap-2 sm:flex hidden justify-between flex-col">
+                <div className="w-[100%] flex justify-between  items-center">
+                  <h6 className=" text-[14px]">Category</h6>
                 </div>
-                <div className="flex bg-[#F4F4F4] py-2 pl-4 pr-3 gap-3 justify-center items-center w-[80px] rounded-lg shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
-                  <h5 className="text-base">Tag</h5>
-                  <IconContext.Provider value={{ size: "15px" }}>
-                    <IoIcons.IoMdClose />
-                  </IconContext.Provider>
-                </div>
-                <div className="flex bg-[#F4F4F4] py-2 pl-4 pr-3 gap-3 justify-center items-center w-[80px] rounded-lg shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
-                  <h5 className="text-base">Tag</h5>
-                  <IconContext.Provider value={{ size: "15px" }}>
-                    <IoIcons.IoMdClose />
-                  </IconContext.Provider>
+                <div className=" flex justify-between  items-center w-full h-full border-[1px] border-[#F4F4F4] rounded-lg px-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
+                  <h6 className=" text-[14px]">Option One</h6>
+                  <PiIcons.PiCaretDownBold />
                 </div>
               </div>
-              <h5 className="text-[14px] text-secondary">Showing 01 of 100</h5>
+              <div className="w-[265px] h-[74px] gap-2 sm:flex hidden justify-between flex-col">
+                <div className="w-[100%] flex justify-between  items-center">
+                  <h6 className=" text-[14px]">Industry/Niche</h6>
+                </div>
+                <div className=" flex justify-between  items-center w-full h-full border-[1px] border-[#F4F4F4] rounded-lg px-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
+                  <h6 className=" text-[14px]">Option One</h6>
+                  <PiIcons.PiCaretDownBold />
+                </div>
+              </div>
+              <div className="w-[265px] h-[74px] gap-2 sm:flex hidden justify-between flex-col">
+                <div className="w-[100%] flex justify-between  items-center">
+                  <h6 className=" text-[14px]">Difficulty Level</h6>
+                </div>
+                <div className=" flex justify-between  items-center w-full h-full border-[1px] border-[#F4F4F4] rounded-lg px-3 shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
+                  <h6 className=" text-[14px]">Option One</h6>
+                  <PiIcons.PiCaretDownBold />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex justify-between items-baseline">
+                <div className="my-8 flex gap-2">
+                  <div className="flex bg-[#F4F4F4] py-2 pl-4 pr-3 gap-3 justify-center items-center w-[80px] rounded-lg shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
+                    <h5 className="text-base">Tag</h5>
+                    <IconContext.Provider value={{ size: "15px" }}>
+                      <IoIcons.IoMdClose />
+                    </IconContext.Provider>
+                  </div>
+                  <div className="flex bg-[#F4F4F4] py-2 pl-4 pr-3 gap-3 justify-center items-center w-[80px] rounded-lg shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
+                    <h5 className="text-base">Tag</h5>
+                    <IconContext.Provider value={{ size: "15px" }}>
+                      <IoIcons.IoMdClose />
+                    </IconContext.Provider>
+                  </div>
+                  <div className="flex bg-[#F4F4F4] py-2 pl-4 pr-3 gap-3 justify-center items-center w-[80px] rounded-lg shadow-[0_1px_2px_0px_rgba(0,0,0,0.06),0_1px_3px_0px_rgba(0,0,0,0.10)]">
+                    <h5 className="text-base">Tag</h5>
+                    <IconContext.Provider value={{ size: "15px" }}>
+                      <IoIcons.IoMdClose />
+                    </IconContext.Provider>
+                  </div>
+                </div>
+                <h5 className="text-[14px] text-secondary">
+                  Showing 01 of 100
+                </h5>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-    
     </div>
   );
 };
