e7da43d4318a3e7235d7ee33cb3e4c6b729052bd
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-09-27 18:41:20 +0100

    commit

--- components/FormWrap.tsx
+++ components/FormWrap.tsx
@@ -2,7 +2,7 @@ const FormWrap = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="">
       <div
-    
+  className='flex flex-col h-[600px]'
       >
         {children}
       </div>
--- components/register/Register.tsx
+++ components/register/Register.tsx
@@ -4,7 +4,7 @@ import RegisterForm from './RegisterForm'
 
 const Register = () => {
   return (
-    <div>
+    <div >
         <FormWrap>
             <RegisterForm/>
         </FormWrap>
