26cbab02d50a5e363263baa6793f7df79f3aca96
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-09-27 20:44:32 +0100

    commit

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
-import  prisma  from "@/libs/prismadb";
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+const prisma = new PrismaClient();
 export async function POST(request: Request) {
   const body = await request.json();
   const { name, email, password } = body;
