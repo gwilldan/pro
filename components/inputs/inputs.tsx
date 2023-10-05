35c3d724ad55e454af999ca4f138d7031415d71d
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-10-05 13:49:04 +0100

    commit

--- components/inputs/inputs.tsx
+++ components/inputs/inputs.tsx
@@ -21,21 +21,35 @@ const Input: React.FC<InputProps> = ({
   register,
   errors,
 }) => {
+  // Regular expression for email validation
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
   return (
-    <div className="w-full relative " >
-      <label className="mb-2 text-base" htmlFor={id}>{label}<sup>*</sup></label>
+    <div className="w-full relative">
+      <label className="mb-2 text-base" htmlFor={id}>
+        {label}
+        {required && <sup>*</sup>}
+      </label>
       <input
         autoComplete="off"
         id={id}
         disabled={disabled}
-        {...register(id, { required })}
+        {...register("email", {
+          required: required ? 'This field is required' : false,
+          pattern: {
+            value: emailRegex,
+            message: 'Invalid email address',
+          },
+        })}
         placeholder=""
         type={type}
-        className={`   className="sm:h-[48px] h-[42px] pl-2 outline-none w-[100%]  border-[1px] border-[#000000] rounded-lg disabled:opacity-70 disabled:cursor-not-allowed ${
+        className={`sm:h-[48px] h-[42px] pl-2 outline-none w-[100%] border-[1px] border-[#000000] rounded-lg disabled:opacity-70 disabled:cursor-not-allowed ${
           errors[id] ? "border-rose-400" : "border-#181818"
         } ${errors[id] ? "focus:border-rose-400" : "focus:border-#181818" }`}
       />
-      
+      {errors[id] && (
+        <span className="text-red-500 text-sm">{errors.email?.message}</span>
+      )}
     </div>
   );
 };
