fe140c7556f43b747b67ae185ff6e0b4c0f2e732
Your Name <okhuomonajayi54@gmail.com>
2023-11-26 19:26:09 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import Searchh from "@/components/Searchh";
 import TasksPage from "@/components/Tasks";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <div className="overflow-hidden">
       <Hero />
 
-      <TasksPage />
+      <Searchh/>
     </div>
   );
 }
--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -2,6 +2,7 @@ import getAllTasks from "@/lib/getAllTasks";
 import Link from "next/link";
 import * as AiIcons from "react-icons/ai";
 import { Button } from "./buttons/Button";
+import Searchh from "./Searchh";
 
 export default async function TasksPage() {
   const tasksData: Promise<Task[]> = getAllTasks();
@@ -28,6 +29,7 @@ export default async function TasksPage() {
     <div>
       <div>
         <div className="sm:px-16 px-4  max-w-[1440px] mx-auto">
+          <Searchh/>
           <div className="grid sm:grid-cols-2 sm:gap-10 gap-5 grid-col-1 ">
             {tasks.map((task) => {
               return (
